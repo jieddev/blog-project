521ea0729db6084bf57073e703b3d473d2a86d5b
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-09 10:48:21 +0800

    Add commented flash message in blog posts controller and display the flash message or notice message in the layouts/application.html.erb

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -33,6 +33,7 @@ class BlogPostsController < ApplicationController
 
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post, notice: "Blog Post update successfully"
+      # flash[:sucess] = "Update successfully"
     else 
       render :edit, status: :unprocessable_entity
     end
