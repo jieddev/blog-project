b21ac634c1c2181ca44a05725bf44beddc496cff
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-11 14:16:32 +0800

    Revert "Add rescue exception in the user#index action if user id does not exist"
    This reverts commit fb633a268373df11cb6ee6177e57909cac6badb3.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,6 @@ class UsersController < ApplicationController
 
   def show 
     @user = User.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      redirect_to blog_posts_path, notice: "No User Found"
   end
 
 
