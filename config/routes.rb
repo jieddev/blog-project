255410077f45a1f6c6cd75aef89ea279bd953f96
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-12 17:53:26 +0800

    Add routes for blog_posts_controller and remove redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
-  get "blog_posts/index"
+  # get "blog_posts/index"
+  resources :blog_posts do 
+    resource :like, only: [:create, :destroy]
+    resource :comments, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
