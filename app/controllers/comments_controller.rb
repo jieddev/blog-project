9a02726ffa2ff8e6eef21e6178071278b930e33f
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-12 12:35:42 +0800

    Refactor create action in the comments controller and initialize the current user using the comment.user and set before action for the set blog post action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,19 @@
 class CommentsController < ApplicationController
+  before_action :set_blog_post, only: [:create]
 
+  def new 
+  end
+
+  def create 
+    @comment = @blog_post.comments.create(body: params[:body])
+    @comment.user = current_user
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to blog_posts_path }
+    end
+
+  end
 
 
 
@@ -9,5 +23,10 @@ class CommentsController < ApplicationController
     def set_blog_post 
       @blog_post = BlogPost.find(params[:blog_post_id])
     end
+
+    def comments_param 
+      params.require(:comments).permit(:body)
+    end
+
 
 end
